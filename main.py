8a13437b1db508f1a545a5af50767898d6d53a84
Bum-Ho12 <bumhonisu@gmail.com>
2023-11-21 14:58:53 +0300

    feature: central runner class for uniformity.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-
-class runApp():
-    pass
+'''
+this file contains the main code to be ran
+for the entire project to execute in python for
+the Pluto framework
+'''
+class RunApp():
+    '''
+    this is the primary method that encapsulates
+    all the widgets of the application.
+    '''

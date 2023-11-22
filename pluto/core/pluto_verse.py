f82a91f38dddcd8052e8b15107729002f6efcca3
Bum-Ho12 <bumhonisu@gmail.com>
2023-11-22 09:57:21 +0300

    fix: handling dependency installment

--- pluto/core/pluto_verse.py
+++ pluto/core/pluto_verse.py
@@ -12,16 +12,18 @@ def install_dependencies(dependencies):
     this method fetches dependencies from internet and installs them in your
     project.
     '''
-    for dependency in dependencies:
-        # pylint: disable = W1510:subprocess-run-check
-        subprocess.run(['pip', 'install', dependency])
+    # pylint: disable = W1510:subprocess-run-check
+    subprocess.run([sys.executable,'-m','pip', 'install','--upgrade'], dependencies)
+    # for dependency in dependencies:
+        # # pylint: disable = W1510:subprocess-run-check
+        # subprocess.run([sys.executable,'-m','pip', 'install','--upgrade'], dependency)
 
 
 
 def create_virtual_environment():
     '''Create a Python virtual environment'''
     # pylint: disable = W1510:subprocess-run-check
-    subprocess.run(['python', '-m', 'venv', 'pluto_env'])
+    subprocess.run([sys.executable, '-m', 'venv', 'pluto_env'])
 
 def activate_virtual_environment():
     '''Activate the Python virtual environment'''
@@ -30,8 +32,16 @@ def activate_virtual_environment():
     # Determine the platform-specific activation script
     activate_script = 'activate' if sys.platform == 'win32' else 'activate'
 
-    activate_script_path = os.path.join(venv_path, 'Scripts'
-                        if sys.platform == 'win32' else 'bin', activate_script)
+    if sys.platform == 'win32':
+        activate_script_path = os.path.join(venv_path,'Scripts',activate_script)
+        os.environ['VIRTUAL_ENV'] = venv_path
+        os.environ['PATH'] = f"{venv_path}/Scripts;{os.environ['PATH']}"
+    else:
+        activate_script_path = os.path.join(venv_path, 'bin', activate_script)
+        os.environ['VIRTUAL_ENV'] = venv_path
+        os.environ['PATH'] = f"{venv_path}/bin:{os.environ['PATH']}"
+    # activate_script_path = os.path.join(venv_path, 'Scripts'
+    #                     if sys.platform == 'win32' else 'bin', activate_script)
 
     # Set the appropriate command based on the platform
     if sys.platform == 'win32':
